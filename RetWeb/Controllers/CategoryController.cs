8c30780aad8e86d869aa165f6848d0969a18eed7
Daniel Tamunotonye <daniel.tamunotonye@purefacts.com>
2023-10-22 15:22:00 -0400

    added category views

--- RetWeb/Controllers/CategoryController.cs
+++ RetWeb/Controllers/CategoryController.cs
@@ -1,6 +1,12 @@
-﻿namespace RetWeb.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RetWeb.Controllers
 {
-    public class CategoryController
+    public class CategoryController : Controller
     {
+        public IActionResult Index()
+        {
+            return View();
+        }
     }
 }
