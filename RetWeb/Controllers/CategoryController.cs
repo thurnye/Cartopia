3fb100eb4061e86e574ab964c80faaf0f70aedea
Daniel Tamunotonye <daniel.tamunotonye@purefacts.com>
2023-10-22 16:21:41 -0400

    displayed all categories

--- RetWeb/Controllers/CategoryController.cs
+++ RetWeb/Controllers/CategoryController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RetWeb.Data;
+using RetWeb.Models;
 
 namespace RetWeb.Controllers
 {
     public class CategoryController : Controller
-    {
+    {   
+        /// <summary>
+        ///  this will help us to connect to the db through the application context since it is available in the services
+        /// </summary>
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _db.Categories.ToList();  // retrieve the list
+            return View(objCategoryList);
         }
     }
 }
