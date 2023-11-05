49a1f049dc78053b97fbf29a59b8e0d106ae765d
Daniel Tamunotonye <daniel.tamunotonye@purefacts.com>
2023-11-05 17:37:19 -0500

    Update Quantity in Cart

--- RetWeb/Areas/Customer/Controllers/CartController.cs
+++ RetWeb/Areas/Customer/Controllers/CartController.cs
@@ -13,11 +13,11 @@ namespace RetWeb.Areas.Customer.Controllers
     public class CartController : Controller
     {
 
-        
+
         private readonly IUnitOfWork _unitOfWork;
         public ShoppingCartVM ShoppingCartVM { get; set; }
 
-        public CartController( IUnitOfWork unitOfWork)
+        public CartController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
         }
@@ -42,7 +42,41 @@ namespace RetWeb.Areas.Customer.Controllers
             return View(ShoppingCartVM);
         }
 
-        private double GetPRiceBasedOnQuantity( ShoppingCart shoppingCart )
+        public IActionResult Plus(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            cartFromDb.Count += 1;
+            _unitOfWork.ShoppingCart.Update(cartFromDb);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Minus(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            if (cartFromDb.Count == 1)
+            {
+                //remove that from Cart
+                _unitOfWork.ShoppingCart.Remove(cartFromDb);
+            }
+            else
+            {
+                cartFromDb.Count -= 1;
+                _unitOfWork.ShoppingCart.Update(cartFromDb);
+            }
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Remove(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            _unitOfWork.ShoppingCart.Remove(cartFromDb);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private double GetPRiceBasedOnQuantity(ShoppingCart shoppingCart)
         {
             if (shoppingCart.Count <= 50)
             {
