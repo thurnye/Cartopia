b4a266996d4de95554dbeb40d28cf5e7ede5d159
Daniel Tamunotonye <daniel.tamunotonye@purefacts.com>
2023-11-06 23:03:39 -0500

    Added the Post action for Order Confirmation

--- RetWeb/Areas/Customer/Controllers/CartController.cs
+++ RetWeb/Areas/Customer/Controllers/CartController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using RetWeb.DataAccess.IRepository;
 using RetWeb.Models;
 using RetWeb.Models.ViewModels;
+using RetWeb.Utility;
 using System.Security.Claims;
 
 
@@ -15,6 +16,7 @@ namespace RetWeb.Areas.Customer.Controllers
 
 
         private readonly IUnitOfWork _unitOfWork;
+        [BindProperty]  //this will automatically populate the shoppingCartVm with values when the post for shoppingCartVM is clicked
         public ShoppingCartVM ShoppingCartVM { get; set; }
 
         public CartController(IUnitOfWork unitOfWork)
@@ -41,6 +43,10 @@ namespace RetWeb.Areas.Customer.Controllers
             return View(ShoppingCartVM);
         }
 
+        /// <summary>
+        /// Get Summary Page
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Summary()
         {
             var userId = GetUserId();   
@@ -75,7 +81,67 @@ namespace RetWeb.Areas.Customer.Controllers
             return View(ShoppingCartVM);
         }
 
-        public IActionResult Plus(int cartId)
+        /// <summary>
+        /// Summary Post 
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [ActionName("Summary")]
+		public IActionResult SummaryPost()
+		{
+			var userId = GetUserId();
+
+            ShoppingCartVM.ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.UserId == userId, includeProperties: "Product");
+
+            ShoppingCartVM.OrderHeader.OrderDate = DateTime.Now;
+			ShoppingCartVM.OrderHeader.UserId = userId;
+			ShoppingCartVM.OrderHeader.User = _unitOfWork.User.Get(u => u.Id == userId);
+
+			foreach (var cart in ShoppingCartVM.ShoppingCartList)
+			{
+				cart.Price = GetPRiceBasedOnQuantity(cart);
+				ShoppingCartVM.OrderHeader.OrderTotal += (cart.Price * cart.Count);
+			}
+
+            //check if user has company Id to give 30 days wait period
+
+            if(ShoppingCartVM.OrderHeader.User.CompanyId.GetValueOrDefault() == 0)
+            {
+                //it is a regular customer account, capture payment
+                ShoppingCartVM.OrderHeader.PaymentStatus = SD.PaymentStatusPending;
+                ShoppingCartVM.OrderHeader.OrderStatus = SD.StatusPending;
+            }
+            else
+            {
+				// it is a company user
+				ShoppingCartVM.OrderHeader.PaymentStatus = SD.PaymentStatusDelayedPayment;
+				ShoppingCartVM.OrderHeader.OrderStatus = SD.StatusApproved;
+			}
+
+            _unitOfWork.OrderHeader.Add(ShoppingCartVM.OrderHeader);
+            _unitOfWork.Save();
+
+			//Order Details
+			foreach (var cart in ShoppingCartVM.ShoppingCartList)
+			{
+                OrderDetail orderDetail = new()
+                {
+                    ProductId = cart.ProductId,
+                    OrderHeaderId = ShoppingCartVM.OrderHeader.Id,
+                    Price = cart.Price,
+                    Count = cart.Count
+                };
+				_unitOfWork.OrderDetail.Add(orderDetail);
+				_unitOfWork.Save();
+			}
+
+			// redirect to a confirmation  page
+		}
+
+
+
+
+		public IActionResult Plus(int cartId)
         {
             var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
             cartFromDb.Count += 1;
