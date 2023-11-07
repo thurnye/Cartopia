2fa5bf48ed6beb3fe924fe54b5c693056e76dc3e
Daniel Tamunotonye <daniel.tamunotonye@purefacts.com>
2023-11-07 15:18:01 -0500

    Show all orders for the admin

---
+++ RetWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,33 @@
+﻿using Cartopia.DataAccess.IRepository;
+using Cartopia.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cartopia.Areas.Admin.Controllers
+{
+	[Area("admin")]
+	public class OrderController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+
+		public OrderController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		#region API Calls 
+		
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "User").ToList();  // retrieve the list
+			return Json(new { data = objOrderHeaders });
+		}
+
+		
+		#endregion
+	}
+}
---
+++ RetWeb/wwwroot/js/order.js
@@ -0,0 +1,28 @@
+﻿let dataTable;
+$(document).ready(() => loadDataTable());
+
+const loadDataTable = () => {
+    dataTable = $('#tblData').DataTable({
+        ajax: { url: '/admin/order/getall' },
+        "columns": [
+            { data: 'id', "width": "5%" },
+            { data: 'name', "width": "25%" },
+            { data: 'phoneNumber', "width": "15%" },
+            { data: 'user.email', "width": "25%" },
+            { data: 'orderStatus', "width": "5%" },
+            { data: 'orderTotal', "width": "10%" },
+            {
+                data: 'id',
+                "width": "15%",
+                "render": (data) => {
+                    return `<div class="w-75 btn-group" role="group">
+                                <a href = "/admin/order/details?orderId=${data}" asp-controller="Order" asp-action="details" asp-route-id="@obj.Id" class=" mx-2">
+                                    <i class="bi bi-pencil-square"></i> 
+                                </a>
+                            </div>`
+                }
+            },
+        ]
+    });
+}
+
