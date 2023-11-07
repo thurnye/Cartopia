28d533013b1d6db8c2a868df471b84161930af34
Daniel Tamunotonye <daniel.tamunotonye@purefacts.com>
2023-11-07 00:35:04 -0500

    Added Helper methods for stripe in OrderHeader Repository

--- RetWeb.DataAccess/Repository/OrderHeaderRepository.cs
+++ RetWeb.DataAccess/Repository/OrderHeaderRepository.cs
@@ -19,5 +19,33 @@ namespace RetWeb.DataAccess.Repository
         {
             _db.OrderHeaders.Update(obj);
         }
-    }
+
+		public void UpdateStatus(int id, string orderStatus, string? paymentStatus = null)
+		{
+            var orderFromDb = _db.OrderHeaders.FirstOrDefault(u => u.Id == id);
+            if(orderFromDb == null)
+            {
+                orderFromDb.OrderStatus = orderStatus;
+
+                if(!string.IsNullOrEmpty(paymentStatus)) 
+                {
+                    orderFromDb.PaymentStatus = paymentStatus;
+                };
+            }
+		}
+
+		public void UpdateStripePaymentID(int id, string sessionId, string? paymentIntentId)
+		{
+			var orderFromDb = _db.OrderHeaders.FirstOrDefault(u => u.Id == id);
+            if(!string.IsNullOrEmpty(sessionId))   // generates when a user tries to make a payment
+            {
+                orderFromDb.SessionId = sessionId;
+            }
+            if(!string.IsNullOrEmpty(paymentIntentId))   // if payment is  successful then a paymentIntentId is generated
+            {
+                orderFromDb.SessionId = sessionId;  //update a successful payment
+                orderFromDb.PaymentDate = DateTime.Now;
+            }
+		}
+	}
 }
