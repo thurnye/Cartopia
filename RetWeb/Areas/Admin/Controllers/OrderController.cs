7e1593bc27f4849759cb3d2d64ec1ed6f886d46a
Daniel Tamunotonye <daniel.tamunotonye@purefacts.com>
2023-11-07 16:36:03 -0500

    added filter based on Payment Status

--- RetWeb/Areas/Admin/Controllers/OrderController.cs
+++ RetWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,6 +1,9 @@
 ﻿using Cartopia.DataAccess.IRepository;
 using Cartopia.Models;
+using Cartopia.Utility;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Security.Claims;
 
 namespace Cartopia.Areas.Admin.Controllers
 {
@@ -21,13 +24,37 @@ namespace Cartopia.Areas.Admin.Controllers
 		#region API Calls 
 		
 		[HttpGet]
-		public IActionResult GetAll()
+		public IActionResult GetAll( string status)
 		{
-			List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "User").ToList();  // retrieve the list
-			return Json(new { data = objOrderHeaders });
+			
+
+            IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "User").ToList();  // retrieve the list based on the status
+			return Json(new { data = GetOrderHeaderByStatus(objOrderHeaders, status) });
 		}
 
 		
+		private IEnumerable<OrderHeader> GetOrderHeaderByStatus(IEnumerable<OrderHeader> objOrderHeaders, string status)
+		{
+            switch (status)
+            {
+                case "pending":
+                  return objOrderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment || u.PaymentStatus == SD.StatusPending);
+                   
+                case "inprocess":
+                    return objOrderHeaders.Where(u => u.OrderStatus == SD.StatusInProcess);
+                    
+                case "completed":
+                    return objOrderHeaders.Where(u => u.OrderStatus == SD.StatusShipped);
+                    
+                case "approved":
+                    return  objOrderHeaders.Where(u => u.OrderStatus == SD.StatusApproved);
+                   
+                default:
+                   return objOrderHeaders;
+            }
+		}
+
 		#endregion
 	}
+
 }
--- RetWeb/wwwroot/js/order.js
+++ RetWeb/wwwroot/js/order.js
@@ -1,9 +1,33 @@
 ﻿let dataTable;
-$(document).ready(() => loadDataTable());
+$(document).ready(function () {
+    var url = window.location.search;
+    if (url.includes("inprocess")) {
+        loadDataTable("inprocess");
+    }
+    else {
+        if (url.includes("completed")) {
+            loadDataTable("completed");
+        }
+        else {
+            if (url.includes("pending")) {
+                loadDataTable("pending");
+            }
+            else {
+                if (url.includes("approved")) {
+                    loadDataTable("approved");
+                }
+                else {
+                    loadDataTable("all");
+                }
+            }
+        }
+    }
 
-const loadDataTable = () => {
+});
+
+const loadDataTable = (status) => {
     dataTable = $('#tblData').DataTable({
-        ajax: { url: '/admin/order/getall' },
+        ajax: { url: '/admin/order/getall?status=' + status },
         "columns": [
             { data: 'id', "width": "5%" },
             { data: 'name', "width": "25%" },
@@ -13,7 +37,7 @@ const loadDataTable = () => {
             { data: 'orderTotal', "width": "10%" },
             {
                 data: 'id',
-                "width": "15%",
+                "width": "10%",
                 "render": (data) => {
                     return `<div class="w-75 btn-group" role="group">
                                 <a href = "/admin/order/details?orderId=${data}" asp-controller="Order" asp-action="details" asp-route-id="@obj.Id" class=" mx-2">
